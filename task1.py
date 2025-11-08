82c0230b0e5a4b895af603cc1604b5ebe046fda8
Harsh <harsh.mailhub@gmail.com>
2025-11-08 11:12:44 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,8 @@
+#  Taking Two numbers as input from user
+num1 = int(input("Enter the first number :"))
+num2 = int(input("Enter the Second number :"))
+
+print("Addition :", num1 + num2)
+print("Subtraction :", num1 - num2)
+print("Multiplication :", num1 * num2)
+print("Division :", num1 / num2)
